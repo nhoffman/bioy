efae81961f24e9fb19c4d1007dbd3457ade52a48
Chris Rosenthal <crosenth@uw.edu>
2013-01-31 15:41:47 -0800

    Calculating a consensus sequence from a multiply aligned fasta file

---
+++ bioy_pkg/subcommands/consensus.py
@@ -0,0 +1,59 @@
+"""
+Calculate the consensus for a multiple aignment
+"""
+
+import logging
+import sys
+import json
+
+from os.path import basename, splitext
+from bioy_pkg.sequtils import consensus, show_consensus, fastalite
+from bioy_pkg.utils import Opener
+
+log = logging.getLogger(__name__)
+
+def build_parser(parser):
+    parser.add_argument('infile',
+            type=Opener(),
+            nargs = '?',
+            default = sys.stdin,
+            help = 'input fasta file (default %(default)s).')
+    parser.add_argument('outfile',
+            type=Opener('w'),
+            default = sys.stdout,
+            nargs = '?',
+            help='Output fasta file.')
+    parser.add_argument('-r','--rlefile',
+            type=Opener(),
+            help='An optional file containing run length encoding for infile (.json.bz2)')
+    parser.add_argument('-n','--seqname',
+            help='Name for the output sequence. Default basename(infile).replace(".fasta","") if infile is provided, otherwise "consensus"')
+    parser.add_argument('--debug',
+            action = 'store_true',
+            default = False,
+            help = 'print detailed description of consensus and exit (with --rlefile option only).')
+    parser.add_argument('--gaps',
+            action = 'store_true',
+            help = 'retain gaps in consensus sequence')
+
+def action(args):
+    if args.seqname:
+        seqname = args.seqname
+    else:
+        seqname = 'consensus' if args.infile.name == '<stdin>' else splitext(basename(args.infile.name))[0]
+
+    seqs = list(fastalite(args.infile, 'fasta'))
+
+    if args.rlefile:
+        rledict = json.load(args.rlefile)
+        rlelist = [rledict[s.id] for s in seqs]
+
+        if args.debug:
+            show_consensus(seqs, rlelist)
+            sys.exit()
+
+        cons = consensus(seqs, rlelist, degap=not args.gaps)
+    else:
+        cons = consensus(seqs, degap=not args.gaps)
+
+    args.outfile.write('>{}\n{}\n'.format(seqname, cons))
