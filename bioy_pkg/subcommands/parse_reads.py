565de4b28383310ff1304582c62b5554f3c5499f
Chris Rosenthal <crosenth@uw.edu>
2013-02-11 09:37:02 -0800

    A reorganized version of the old ion parse_reads.py

--- bioy_pkg/subcommands/parse_reads.py
+++ bioy_pkg/subcommands/parse_reads.py
@@ -3,14 +3,15 @@ Parse barcode, primer, and read from a fastq file
 """
 
 import logging
+import os
 import csv
+from itertools import islice, groupby
 import sys
 
-from itertools import islice, groupby
-from os.path import join, basename, splitext, dirname
+from Bio import SeqIO
 
-from bioy_pkg.sequtils import parse_ssearch36, parse_primer_alignments, fastalite
-from bioy_pkg.utils import opener, Opener, Csv2Dict
+from bioy_pkg.sequtils import parse_ssearch36, parse_primer_alignments
+from bioy_pkg.utils import Opener, Csv2Dict
 
 log = logging.getLogger(__name__)
 
@@ -18,29 +19,39 @@ def build_parser(parser):
     parser.add_argument('fasta',
             type = Opener(),
             help = 'input file containing raw reads')
-    parser.add_argument('--mapfile',
-            type = Csv2Dict('name'),
-            help = 'csv file containing read name (first column) and specimen (second column); a header row is assumed. Overwrites --rle-file, --out')
-    parser.add_argument('--primer-aligns',
+    parser.add_argument('primer_aligns',
             type = Opener(),
             help = 'ssearch36 output (may be bz2-encoded)')
     parser.add_argument('-l','--keep-left',
             help = 'python expression defining criteria for keeping left primer',
-            default = "d.get('start') <= 100 and d.get('sw_zscore') > 100")
+            default = "0 <= d.get('start') <= 25 and d.get('sw_zscore') > 50")
     parser.add_argument('-r','--keep-right',
             help = 'python expression defining criteria for keeping left primer',
-            default = "200 <= d.get('start') and d.get('sw_zscore') > 100")
-    parser.add_argument('--rle',
-            type = Csv2Dict('name'),
-            help = 'run length encoded file')
+            default = "200 <= d.get('start') <= 320 and d.get('sw_zscore') > 50")
+    parser.add_argument('-d','--outdir',
+            help = 'output directory',
+            default = '.')
     parser.add_argument('--limit',
             type = int,
             help = 'maximum number of query sequences to read from the alignment')
-    parser.add_argument('-o', '--out',
-            help = """output file for fasta reads.  If not mapfile is specified the
-                      basename of this file will serve as the basename for the rle file as well.""")
+    parser.add_argument('-o', '--out-fasta',
+            type = Opener('w'),
+            default = sys.stdout,
+            help = 'trimmed fasta output file')
+    parser.add_argument('--rle',
+            type = Csv2Dict('name'),
+            help = 'rle file')
+    parser.add_argument('-O', '--out-rle',
+            type = Opener('w'),
+            default = sys.stdout,
+            help = 'trimmed rle output file')
 
 def action(args):
+    try:
+        os.mkdir(args.outdir)
+    except OSError:
+        pass
+
     # I only want eval() to happen once...
     def make_fun(expression):
         return lambda d: eval(expression)
@@ -50,38 +61,19 @@ def action(args):
 
     # parse primer alignments
     primerdict = {}
-    for q_name, hits in islice(groupby(parse_ssearch36(args.primer_aligns),
-        lambda hit: hit['q_name']), args.limit):
+    for q_name, hits in islice(groupby(parse_ssearch36(args.primer_aligns), lambda hit: hit['q_name']), args.limit):
         primerdict[q_name] = parse_primer_alignments(hits, lprimer = 'lprimer', rprimer = 'rprimer')
 
     # a place to put open file handles
-    fafiles = {}
-    rlewriters = {}
-    if args.mapfile:
-        for sample in set(x['label'] for x in args.mapfile.values()) - {''}:
-            fafiles[sample] = opener(join(args.outdir,'{}.fasta'.format(sample)), 'w')
-            rlefile = opener(join(args.outdir, '{}.csv.bz2'.format(sample)), 'w')
-            rlewriters[sample] = csv.writer(rlefile)
-            rlewriters[sample].writerow(['name','rle'])
-    else:
-        fa_base,_ = splitext(args.out)
-        fa_name = basename(fa_base)
-        dir_name = dirname(fa_base)
-        fafiles[fa_name] = opener(join(dir_name, '{}.fasta'.format(fa_name)), 'w')
-        rlewriters[fa_name] = csv.writer(opener(join(dir_name, '{}.csv.bz2'.format(fa_name)), 'w'))
-        rlewriters[fa_name].writerow(['name','rle'])
+    writer = csv.writer(args.out_rle)
+    writer.writerow(['name','rle'])
 
     # parse the sequences
-    for seq in islice(fastalite(args.fasta, readfile = False), args.limit):
-        pdict = primerdict[seq.description]
-        start = pdict['l']['stop'] if keep_left(pdict['l']) else 0
-        stop = pdict['r']['start'] if keep_right(pdict['r']) else len(seq.seq)
-
-        if args.mapfile:
-            sample, rle = args.mapfile[seq.description]
-        else:
-            sample, rle = fa_name, args.rle[seq.description]
-
-        fafiles[sample].write('>{}\n{}\n'.format(seq.description, str(seq.seq)[start:stop]))
-        rlewriters[sample].writerow([seq.description, rle[start:stop]])
+    for seq in islice(SeqIO.parse(args.fasta, 'fasta'), args.limit):
+        pdict = primerdict[seq.name]
+        if keep_left(pdict['l']) and keep_right(pdict['r']):
+            start, stop = pdict['l']['stop'], pdict['r']['start']
+            args.out_fasta.write(
+                '>%s\n%s\n' % (seq.name, str(seq.seq)[start:stop]))
+            writer.writerow([seq.name, args.rle[seq.name][start:stop]])
 
