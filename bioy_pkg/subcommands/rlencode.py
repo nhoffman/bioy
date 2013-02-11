307d07d37e7ad813a7aef2538efb5997e6e6e0e4
Chris Rosenthal <crosenth@uw.edu>
2013-02-11 09:38:30 -0800

    Using fasta out as basename for rle file

--- bioy_pkg/subcommands/rlencode.py
+++ bioy_pkg/subcommands/rlencode.py
@@ -3,6 +3,9 @@
 import logging
 import csv
 import sys
+from concurrent.futures import ProcessPoolExecutor
+
+from os.path import splitext
 
 from bioy_pkg.sequtils import homoencode, to_ascii, fastalite
 from bioy_pkg.utils import Opener, opener
@@ -17,21 +20,30 @@ def build_parser(parser):
             help = 'Input fasta file')
     parser.add_argument('-o','--outfile',
             type = Opener('w'),
-            help = 'Name of output file; default is to append _rle.fasta to basename.')
+            default = sys.stdout,
+            help = 'Name of output file; default: %(default)s')
     parser.add_argument('-r','--rlefile',
             type = Opener('w'),
-            help = 'Name of output file for run length encoding; default is to append _rle.csv.bz2 to basename.')
+            help = """Name of output file for run length encoding; default is to
+                      append .csv.bz2 to --outfile basename.""")
 
 def action(args):
-    seqs = fastalite(args.infile)
+    if args.rlefile:
+        rlefile = args.rlefile
+    elif args.outfile and args.outfile is not sys.stdout:
+        rlefile = opener('{}.{}'.format(splitext(args.outfile.name)[0], 'csv.bz2'), 'w')
+    else:
+        rlefile = None
 
-    outfile = args.outfile or opener(args.infile.name.replace('.fasta', '_rle.fasta', 'w'))
-    rlefile = args.rlefile or opener(args.infile.name.replace('.fasta', '_rle.csv.bz2'), 'w')
+    counts = []
+    for seq in fastalite(args.infile):
+        seqstr, count = homoencode(seq.seq)
+        assert len(seqstr) == len(count)
+        args.outfile.write('>{}\n{}\n'.format(seq.description, seqstr))
+        counts.append([seq.description, to_ascii(count)])
 
-    writer = csv.writer(rlefile)
-    writer.writerow(['name','rle'])
-    for seq in seqs:
-        seqstr, counts = homoencode(seq.seq)
-        assert len(seqstr) == len(counts)
-        outfile.write('>{}\n{}\n'.format(seq.description, seqstr))
-        writer.writerow([seq.description, to_ascii(counts)])
+    if rlefile:
+        writer = csv.writer(rlefile)
+        writer.writerow(['name', 'rle'])
+        writer.writerows(counts)
+
