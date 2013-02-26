29aa2f74a97f536a5f7a39a013da972bc170a429
Chris Rosenthal <crosenth@uw.edu>
2013-02-26 11:48:43 -0800

    Allowing any number of fasta file inputs.  User must specify counts file.

--- bioy_pkg/subcommands/rlencode.py
+++ bioy_pkg/subcommands/rlencode.py
@@ -3,19 +3,18 @@
 import logging
 import csv
 import sys
-from concurrent.futures import ProcessPoolExecutor
 
-from os.path import splitext
+from itertools import imap, chain
 
 from bioy_pkg.sequtils import homoencode, to_ascii, fastalite
-from bioy_pkg.utils import Opener, opener
+from bioy_pkg.utils import Opener
 
 log = logging.getLogger(__name__)
 
 def build_parser(parser):
-    parser.add_argument('infile',
-            nargs = '?',
-            default = sys.stdin,
+    parser.add_argument('infiles',
+            nargs = '*',
+            default = [sys.stdin],
             type = Opener(),
             help = 'Input fasta file')
     parser.add_argument('-o','--outfile',
@@ -28,22 +27,15 @@ def build_parser(parser):
                       append .csv.bz2 to --outfile basename.""")
 
 def action(args):
-    if args.rlefile:
-        rlefile = args.rlefile
-    elif args.outfile and args.outfile is not sys.stdout:
-        rlefile = opener('{}.{}'.format(splitext(args.outfile.name)[0], 'csv.bz2'), 'w')
-    else:
-        rlefile = None
-
     counts = []
-    for seq in fastalite(args.infile):
+    for seq in chain.from_iterable(imap(fastalite, args.infiles)):
         seqstr, count = homoencode(seq.seq)
         assert len(seqstr) == len(count)
         args.outfile.write('>{}\n{}\n'.format(seq.description, seqstr))
         counts.append([seq.description, to_ascii(count)])
 
-    if rlefile:
-        writer = csv.writer(rlefile)
+    if args.rlefile:
+        writer = csv.writer(args.rlefile)
         writer.writerow(['name', 'rle'])
         writer.writerows(counts)
 
