4b543a31c018243f11daa6fe39f481c26e2b1e26
Chris Rosenthal <crosenth@uw.edu>
2013-04-16 11:57:38 -0700

    NOt completed but need to pull from nhoffman to continue development

--- bioy_pkg/subcommands/errors.py
+++ bioy_pkg/subcommands/errors.py
@@ -1,8 +1,5 @@
 """
 Tally and classify errors given ./ion rlaligns reference and query sequences
-
-Alignments are provided in a csv file with columns ["q_name",
-"t_name", "q_align", "t_align"]
 """
 
 import logging
@@ -10,19 +7,21 @@ import sys
 
 from collections import Counter
 from csv import DictWriter, writer, DictReader
+from itertools import imap, tee
+from os import devnull
+
 from bioy_pkg import sequtils
 from bioy_pkg.sequtils import itemize_errors, error_count
 from bioy_pkg.utils import Opener, parse_extras
-from os import devnull
 
 log = logging.getLogger(__name__)
 
 def build_parser(parser):
-    parser.add_argument('alignments',
+    parser.add_argument('aligns',
             nargs = '?',
             default = sys.stdin,
             type = Opener('r'),
-            help = 'csvfile of errors in format ["q_name", "t_name", "q_align", "t_align"]')
+            help = 'csvfile of ssearch results')
     parser.add_argument('-o', '--out',
             default = sys.stdout,
             type = Opener('w'),
@@ -39,45 +38,61 @@ def build_parser(parser):
             action = 'store_true',
             help = 'step through reults (for debugging)')
     parser.add_argument('-f', '--extra-fields',
+            type = lambda f: parse_extras(f),
+            default = {},
             help="extra fields for csv file in form 'name1:val1,name2:val2'")
     parser.add_argument('--output-alignment',
             action='store_true',
             help = 'Include the actual alignment in csv output')
 
+def tallie_errors(align, alignment = False):
+    errors = itemize_errors(align['t_seq'], align['q_seq'])
+    errors['q_name'] = align['q_name']
+    errors['t_name'] = align['t_name']
+
+    if alignment:
+
+
+    return errors
+
 def action(args):
-    extras = parse_extras(args.extra_fields) if args.extra_fields else {}
-    fieldnames = ['t_name', 'q_name', 'length', 'snp', 
-            'indel', 'homoindel','compound'] + extras.keys()
+    fieldnames = ['t_name', 'q_name', 'length', 'snp']
+    fieldnames += ['indel', 'homoindel','compound']
+    fieldnames += args.extra_fields.keys()
 
     if args.output_alignment:
-        fieldnames.append('alignment')
+        fieldnames += ['alignment']
 
-    # ssearch query should have been executed with reads as query and
-    # reference sequences as target. So each query may have alignments
-    # for multiple targets. Therefore, we group by and take only the
-    # first (top) hit for each
+    aligns = DictReader(args.aligns)
+    errors = imap(tallie_errors, aligns)
+    errors, matrix = tee(errors)
+
 
     # Set up homopolymer matrix vars:
     cnt = Counter()
     gtceil = 'geq{}'.format(args.homopolymer_max)
     ###
 
-    tallies = DictWriter(args.out, fieldnames=fieldnames, extrasaction='ignore')
+    tallies = DictWriter(args.out,
+            fieldnames = fieldnames,
+            extrasaction = 'ignore')
     tallies.writeheader()
 
-    for a in DictReader(args.alignments):
-        e = list(itemize_errors(a['t_align'], a['q_align']))
+    for a in aligns:
+        e = itemize_errors(a['t_seq'], a['q_seq'])
 
         # instantiate d with zero counts for each error type
         d = {k:0 for k in fieldnames[2:]}
         d['q_name'], d['t_name'] = a['q_name'], a['t_name']
         d.update(error_count(e))
-        d.update(extras)
+        d.update(args.extra_fields)
 
         if args.output_alignment:
             d.update({'alignment':sequtils.show_errors(e)})
 
         tallies.writerow(d)
+
+
 
         for d in e:
             r, q = d['ref'].strip('=-'), d['query'].strip('=-')
@@ -87,8 +102,8 @@ def action(args):
                     len(q) if len(q) <= args.homopolymer_max else gtceil)] += 1
 
         log.debug(a['q_name'])
-        log.debug('\n' + sequtils.format_alignment(a['t_align'], a['q_align']))
-        log.debug(a['q_align'].replace('-','').replace('=',''))
+        log.debug('\n' + sequtils.format_alignment(a['t_seq'], a['q_seq']))
+        log.debug(a['q_seq'].replace('-','').replace('=',''))
         log.debug(sequtils.show_errors(e))
 
         if args.step:
