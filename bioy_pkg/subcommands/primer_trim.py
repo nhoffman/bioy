6b34c0125357981860b6e9959cacf16100106676
Chris Rosenthal <crosenth@uw.edu>
2013-05-03 12:43:04 -0700

    Need to coerce primer location from string to int

--- bioy_pkg/subcommands/primer_trim.py
+++ bioy_pkg/subcommands/primer_trim.py
@@ -69,9 +69,9 @@ def primer_dict(parsed, side, keep = None, include = False):
 
     positions = {
         ('left', False): lambda hit: hit['q_al_stop'],
-        ('left', True): lambda hit: hit['q_al_start'] - 1,
+        ('left', True): lambda hit: int(hit['q_al_start']) - 1,
         ('right', True): lambda hit: hit['q_al_stop'],
-        ('right', False): lambda hit: hit['q_al_start'] - 1
+        ('right', False): lambda hit: int(hit['q_al_start']) - 1
     }
 
     # 'best' hit is assumed to be first in each group
