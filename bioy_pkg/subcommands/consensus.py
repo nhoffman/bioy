37d7ab5edf8d8a116008e78cabeb476af27d5f1a
Noah Hoffman <noah.hoffman@gmail.com>
2013-05-07 21:14:28 -0700

    remove import of missing sequtils.show_consensus

--- bioy_pkg/subcommands/consensus.py
+++ bioy_pkg/subcommands/consensus.py
@@ -7,10 +7,11 @@ import sys
 import json
 
 from os.path import basename, splitext
-from bioy_pkg.sequtils import consensus, show_consensus, fastalite
+from bioy_pkg.sequtils import consensus, fastalite
 from bioy_pkg.utils import Opener
 
 log = logging.getLogger(__name__)
+
 
 def build_parser(parser):
     parser.add_argument('infile',
@@ -23,35 +24,31 @@ def build_parser(parser):
             default = sys.stdout,
             nargs = '?',
             help='Output fasta file.')
-    parser.add_argument('-r','--rlefile',
+    parser.add_argument('-r', '--rlefile',
             type=Opener(),
-            help='An optional file containing run length encoding for infile (.json.bz2)')
-    parser.add_argument('-n','--seqname',
-            help='Name for the output sequence. Default basename(infile).replace(".fasta","") if infile is provided, otherwise "consensus"')
-    parser.add_argument('--debug',
-            action = 'store_true',
-            default = False,
-            help = 'print detailed description of consensus and exit (with --rlefile option only).')
+            help=('An optional file containing run length '
+                  'encoding for infile (.json.bz2)'))
+    parser.add_argument('-n', '--seqname',
+            help=('Name for the output sequence. '
+                  'Default basename(infile).replace(".fasta","") '
+                  'if infile is provided, otherwise "consensus"'))
     parser.add_argument('--gaps',
             action = 'store_true',
             help = 'retain gaps in consensus sequence')
+
 
 def action(args):
     if args.seqname:
         seqname = args.seqname
     else:
-        seqname = 'consensus' if args.infile is sys.stdin else splitext(basename(args.infile.name))[0]
+        seqname = 'consensus' if args.infile is sys.stdin \
+                  else splitext(basename(args.infile.name))[0]
 
     seqs = list(fastalite(args.infile, 'fasta'))
 
     if args.rlefile:
         rledict = json.load(args.rlefile)
         rlelist = [rledict[s.id] for s in seqs]
-
-        if args.debug:
-            show_consensus(seqs, rlelist)
-            sys.exit()
-
         cons = consensus(seqs, rlelist, degap=not args.gaps)
     else:
         cons = consensus(seqs, degap=not args.gaps)
