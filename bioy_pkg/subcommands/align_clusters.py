2ad6fc94d15342a16e797742bf49898f1a871553
Noah Hoffman <noah.hoffman@gmail.com>
2013-05-09 16:42:33 -0700

    add --no-align option

--- bioy_pkg/subcommands/align_clusters.py
+++ bioy_pkg/subcommands/align_clusters.py
@@ -18,26 +18,30 @@ from bioy_pkg.utils import chunker, Opener, Csv2Dict
 
 log = logging.getLogger(__name__)
 
-
 def build_parser(parser):
     parser.add_argument('raw_reads',
-            type = lambda f: fastalite(Opener()(f)),
-            help = """input fasta file containing original
-            clustered reads (default stdin).""")
+                        type = lambda f: fastalite(Opener()(f)),
+                        help = """input fasta file containing original
+                        clustered reads (default stdin).""")
     parser.add_argument('readmap',
-            type = Opener('r'),
-            help = """output of `bioy denoise --readmap`
-            (csv file with columns readname,clustername)""")
+                        type = Opener('r'),
+                        help = """output of `bioy denoise --readmap`
+                        (csv file with columns readname,clustername)""")
     parser.add_argument('-r', '--rlefile',
-            type = Csv2Dict('name', 'rle', fieldnames=['name', 'rle']),
-            help="""An optional file containing run
-            length encoding for infile (.csv.bz2)""")
+                        type = Csv2Dict('name', 'rle', fieldnames=['name', 'rle']),
+                        help="""An optional file containing run
+                        length encoding for infile (.csv.bz2)""")
     parser.add_argument('-d', '--outdir', help='output directory', default='.')
     parser.add_argument('--pattern',
-            help = """A regular expression matching cluster names""")
+                        help = """A regular expression matching cluster names""")
     parser.add_argument('-N', '--sample', type=int, default=100,
                         metavar='N',
                         help='inculde no more than N reads [%(default)s]')
+    parser.add_argument('--name-suffix',
+                        help='string to insert into name before .fasta',
+                        default='aln')
+    parser.add_argument('--no-align',
+                        action='store_false', dest='align', default=True)
 
 
 def action(args):
@@ -52,16 +56,23 @@ def action(args):
     for cons, group in groups:
         if args.pattern and not re.search(r'' + args.pattern, cons):
             continue
-        log.info(cons)
-        reads, _ = zip(*group)
-        seqs = [seqdict[name] for name in reads]
-        if len(seqs) > args.sample:
-            seqs = random.sample(seqs, args.sample)
-        if args.rlefile:
-            seqs = (rlemap(s) for s in seqs)
-            with fasta_tempfile(seqs) as f:
-                outfile = path.join(args.outdir, '{}.aln.fasta'.format(cons))
-                command = ['muscle', '-quiet', '-seqtype', 'dna',
-                           '-in', f, '-out', outfile]
-                log.debug(' '.join(command))
-                subprocess.check_call(command)
+            log.info(cons)
+            reads, _ = zip(*group)
+            seqs = [seqdict[name] for name in reads]
+            if len(seqs) > args.sample:
+                seqs = random.sample(seqs, args.sample)
+                if args.rlefile:
+                    seqs = (rlemap(s) for s in seqs)
+                    outfile = path.join(
+                        args.outdir,
+                        '{}.{}.fasta'.format(cons, args.name_suffix))
+
+            if args.align:
+                with fasta_tempfile(seqs) as f:
+                    command = ['muscle', '-quiet', '-seqtype', 'dna',
+                               '-in', f, '-out', outfile]
+                    log.debug(' '.join(command))
+                    subprocess.check_call(command)
+                else:
+                    with open(outfile, 'w') as f:
+                        f.write('\n'.join('>{}\n{}'.format(s.id, s.seq) for s in seqs))
