0f9e5c4485bae82ff00b0189d901a21ba44dccd0
Chris Rosenthal <crosenth@uw.edu>
2013-06-18 13:53:20 -0700

    multiprocessing for about a 1/3 speed up

--- bioy_pkg/subcommands/rlencode.py
+++ bioy_pkg/subcommands/rlencode.py
@@ -1,10 +1,12 @@
 """Run-length encode a fasta file"""
 
 import logging
+import os
 import sys
 
 from itertools import imap, chain
 from csv import DictWriter
+from multiprocessing import Pool
 
 from bioy_pkg.sequtils import homoencode, to_ascii, fastalite
 from bioy_pkg.utils import Opener
@@ -25,10 +27,24 @@ def build_parser(parser):
             type = lambda f: DictWriter(Opener('w')(f), fieldnames=['name', 'rle']),
             help = """Name of output file for run length encoding; default is to
                       append .csv.bz2 to --outfile basename.""")
+    parser.add_argument('--threads',
+            default = int(os.environ.get('THREADS_ALLOC') or 1),
+            type = int,
+            help = """Number of threads (CPUs) to use.
+                   Can also specify with environment variable THREADS_ALLOC
+                   default = %(default)s""")
+
+def seq_and_homoencode(seq):
+    return seq, homoencode(seq.seq)
 
 def action(args):
-    for seq in chain.from_iterable(imap(fastalite, args.infiles)):
-        seqstr, count = homoencode(seq.seq)
+    pool = Pool(processes = args.threads)
+
+    seqs = imap(fastalite, args.infiles)
+    seqs = chain.from_iterable(seqs)
+    seqs = pool.imap(seq_and_homoencode, seqs, chunksize = 40)
+
+    for seq, (seqstr, count) in seqs:
         assert len(seqstr) == len(count)
         args.outfile.write('>{}\n{}\n'.format(seq.description, seqstr))
         if args.rlefile:
