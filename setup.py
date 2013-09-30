6a5a0398d6a6cdd2db558f06f9767553fea68369
Chris Rosenthal <crosenth@uw.edu>
2013-09-30 15:40:45 -0700

    still trying to lineup package info

--- setup.py
+++ setup.py
@@ -16,10 +16,9 @@ from bioy_pkg import __version__
 params = {'author': 'Noah Hoffman',
           'author_email': 'ngh2@uw.edu',
           'description': 'A collection of bioinformatics tools',
-          'name': 'bioy_pkg',
+          'name': 'bioy-pkg',
           'packages': find_packages(),
           'version': __version__,
-          'package_data': {'bioy': [join('data',f) for f in ['sha','ver']]}
           }
 
 setup(**params)
