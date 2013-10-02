e20c62e0325875b0c894a82f54ca4174e7f91a04
Chris Rosenthal <crosenth@uw.edu>
2013-10-02 14:43:22 -0700

    adding additional setup.py information

--- setup.py
+++ setup.py
@@ -5,22 +5,20 @@ Setup.py template. Try this:
 """
 
 import subprocess
+
 from setuptools import setup, find_packages
-from os.path import join
 
 subprocess.call('git log --pretty=format:%h -n 1 > bioy_pkg/data/sha', shell = True)
 subprocess.call('git shortlog --format="XXYYXX%h" | grep -c XXYYXX > bioy_pkg/data/ver', shell = True)
 
-from bioy_pkg import __version__
+version = subprocess.check_output('git describe --tags', shell = True).strip()
 
-params = {'author': 'Noah Hoffman',
-          'author_email': 'ngh2@uw.edu',
-          'description': 'A collection of bioinformatics tools',
-          'name': 'bioy',
-          'packages': find_packages(),
-          'scripts': ['bioy'],
-          'version': __version__,
-          }
-
-setup(**params)
+setup(author = 'Noah Hoffman',
+      author_email = 'ngh2@uw.edu',
+      description = 'A collection of bioinformatics tools',
+      name = 'bioy',
+      packages = find_packages(),
+      scripts = ['bioy'],
+      version = version,
+      url = 'https://bitbucket.org/uwlabmed/bioy/')
 
