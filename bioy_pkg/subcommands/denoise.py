c5ec48880fa5c80e799f9df7bbf582959d5cdc93
Noah Hoffman <noah.hoffman@gmail.com>
2013-10-28 15:44:24 -0700

    update parser help text

--- bioy_pkg/subcommands/denoise.py
+++ bioy_pkg/subcommands/denoise.py
@@ -31,37 +31,37 @@ def build_parser(parser):
     parser.add_argument('clusters',
             type = Opener(),
             help = 'Clusters file (output of "usearch -uc")')
-    parser.add_argument('--specimen',
-            help = 'sample name for mapfile')
-    parser.add_argument('-r','--rlefile',
+    parser.add_argument('-r','--rlefile', metavar='FILE',
             type = Csv2Dict('name', 'rle', fieldnames=['name','rle']),
             help='An optional file containing run length encoding for infile (.csv.bz2)')
-    parser.add_argument('-o','--outfile',
+    parser.add_argument('-o','--outfile', metavar='FILE',
             type = Opener('w'),
             default = sys.stdout,
             help='Output fasta file.')
-    parser.add_argument('--readmap',
+    parser.add_argument('--readmap', metavar='FILE',
             type = lambda f: csv.writer(Opener('w')(f)),
             help = 'Output file with columns (readname,clustername)')
-    parser.add_argument('--clustermap',
+    parser.add_argument('--clustermap', metavar='FILE',
             type = lambda f: csv.writer(Opener('w')(f)),
-            help = 'Output file with columns (clustername,samplename)')
-    parser.add_argument('-w', '--weights',
+                        help = 'Output file with columns (clustername,specimen)')
+    parser.add_argument('--specimen', metavar='NAME',
+            help = 'value for second column of --clustermap')
+    parser.add_argument('-w', '--weights', metavar='FILE',
             type = lambda f: csv.writer(Opener('w')(f)),
             help = 'Output file with columns (clustername,weight)')
-    parser.add_argument('--max-clust-size',
+    parser.add_argument('--max-clust-size', metavar='INTEGER',
             type = int,
             default = sys.maxint, help = 'default %(default)s')
-    parser.add_argument('--min-clust-size',
+    parser.add_argument('--min-clust-size', metavar='INTEGER',
             type = int,
             default = 1, help = 'default %(default)s')
     parser.add_argument('--limit',
-            metavar = 'N',
+            metavar = 'INTEGER',
             type = int,
             help = 'use no more than N seqs')
-    parser.add_argument('--name-prefix',
+    parser.add_argument('--name-prefix', metavar='STRING',
             help = 'A string to prepend to each cluster name.')
-    parser.add_argument('--name-suffix',
+    parser.add_argument('--name-suffix', metavar='STRING',
             help = 'A string to append to each cluster name.')
     parser.add_argument('--name-delimiter', default = CLUSTER_NAME_DELIMITER,
                         metavar = 'CHAR',
