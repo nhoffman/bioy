6d2328cc36fe6ee231a6b7c23c3f90c9e780604c
Chris Rosenthal <crosenth@uw.edu>
2014-01-21 11:13:58 -0800

    defaulting to max cpus for thread allocation

--- bioy_pkg/scripts/globe.py
+++ bioy_pkg/scripts/globe.py
@@ -1,9 +1,10 @@
 import os
+from multiprocessing import cpu_count
 
 def parse_args(parser):
     parser.add_argument('--threads',
             metavar = 'NUM',
-            default = int(os.environ.get('THREADS_ALLOC') or 1),
+            default = int(os.environ.get('THREADS_ALLOC') or cpu_count()),
             type = int,
             help = """Number of threads (CPUs). Can also specify
                       with environment variable THREADS_ALLOC.
