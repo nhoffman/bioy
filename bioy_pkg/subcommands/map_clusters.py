1025e15598845a031f07c41f7890881303c7dd88
Noah Hoffman <noah.hoffman@gmail.com>
2014-04-20 21:11:00 -0700

    pep8

--- bioy_pkg/subcommands/map_clusters.py
+++ bioy_pkg/subcommands/map_clusters.py
@@ -10,7 +10,6 @@ import sys
 import csv
 import os
 
-from itertools import groupby, ifilterfalse
 from operator import itemgetter
 
 from bioy_pkg.sequtils import UCLUST_HEADERS, fastalite
@@ -18,24 +17,34 @@ from bioy_pkg.utils import Opener, groupbyl
 
 log = logging.getLogger(__name__)
 
+
 def build_parser(parser):
-    parser.add_argument('fastafile',
-            type = lambda f: fastalite(Opener()(f), readfile = False),
-            help = 'input fasta file containing original clustered reads')
-    parser.add_argument('clusters', type = Opener(),
-            help = 'Clusters file (output of "usearch -uc")')
-    parser.add_argument('-o','--out', type = Opener('w'), default = sys.stdout,
-            help = 'Output fasta containing centroids')
-    parser.add_argument('--readmap', type = Opener('w'),
-            help = 'Output file with columns (readname,centroidname)')
-    parser.add_argument('--clustermap', type = Opener('w'),
-            help = 'Output file with columns (clustername,samplename)')
-    parser.add_argument('--specimen', metavar='SAMPLENAME',
-            help = 'provides samplename for mapfile')
-    parser.add_argument('-w', '--weights', type = Opener('w'),
-            help = 'Output file with columns (clustername,weight)')
-    parser.add_argument('--min-clust-size', type = int,
-            default = 1, help = 'default %(default)s')
+    parser.add_argument(
+        'fastafile', type=lambda f: fastalite(
+            Opener()(f), readfile=False),
+        help='input fasta file containing original clustered reads')
+    parser.add_argument(
+        'clusters', type=Opener(),
+        help='Clusters file (output of "usearch -uc")')
+    parser.add_argument(
+        '-o', '--out', type=Opener('w'), default=sys.stdout,
+        help='Output fasta containing centroids')
+    parser.add_argument(
+        '--readmap', type=Opener('w'),
+        help='Output file with columns (readname,centroidname)')
+    parser.add_argument(
+        '--clustermap', type=Opener('w'),
+        help='Output file with columns (clustername,samplename)')
+    parser.add_argument(
+        '--specimen', metavar='SAMPLENAME',
+        help='provides samplename for mapfile')
+    parser.add_argument(
+        '-w', '--weights', type=Opener('w'),
+        help='Output file with columns (clustername,weight)')
+    parser.add_argument(
+        '--min-clust-size', type=int,
+        default=1, help='default %(default)s')
+
 
 def get_mapping(rows):
     """
