30ee4654684dcd36519e3afdddcbbc0026089fb9
Noah Hoffman <noah.hoffman@gmail.com>
2014-05-09 09:24:42 -0700

    minor cleanup

--- tests/test_fastq_stats.py
+++ tests/test_fastq_stats.py
@@ -5,30 +5,28 @@ import filecmp
 import logging
 from os import path
 
-from bioy_pkg.subcommands import fastq_stats as subcommand
+from bioy_pkg.subcommands import fastq_stats
 from __init__ import TestBase, TestSubcommand, datadir
 
 log = logging.getLogger(__name__)
 
 class TestFastq_stats(TestBase, TestSubcommand):
 
-    subcommand = subcommand
-    #bioy 'fastq_stats {}'
+    subcommand = fastq_stats
     fq_in = path.join(datadir, '16S_random.fastq')
 
     def test01(self):
         """
-        Basic comparison of output
+        Regression test for a simple test file.
         """
 
-        #datadir = self.datadir
         outdir = self.mkoutdir()
         fq_out = path.join(outdir, 'fastq_stats.csv')
         reference = path.join(datadir, '16S_random.csv')
         args = ['--out', fq_out, self.fq_in]
         self.main(args)
-        print(open(fq_out))
-        #log.info(self.log_info.format(' '.join(map(str, args))))
         self.assertTrue(filecmp.cmp(reference, fq_out))
 
         #this_test = sys._getframe().f_code.co_name
+
+
