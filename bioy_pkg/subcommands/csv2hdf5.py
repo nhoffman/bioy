cd782c6b3508b99b25f2f9d1d49d044fdd852179
Noah Hoffman <noah.hoffman@gmail.com>
2014-05-16 21:26:23 -0700

    csv2hdf5 more or less usable

--- bioy_pkg/subcommands/csv2hdf5.py
+++ bioy_pkg/subcommands/csv2hdf5.py
@@ -9,6 +9,7 @@ import re
 import sys
 
 import pandas as pd
+import numpy as np
 
 from bioy_pkg.utils import Opener
 
@@ -25,19 +26,16 @@ def build_parser(parser):
     parser.add_argument('-d', '--outdir',
                         help = 'Optional output directory. Ignored if -o/--outfile is specified.')
     parser.add_argument('--fieldnames', help='comma-delimited list of field names.')
-    parser.add_argument('--headless', action='store_true', default=False,
+    parser.add_argument('-H', '--no-header', action='store_true', default=False,
                         help="""indicate that the input file has no
                         header row. Uses value of --fieldnames if provided.""")
     parser.add_argument('-k', '--key', default='data',
                         help='A label identifing this table in the data store "[%(default)s]"')
-    parser.add_argument('-c', '--compress', action='store_true', default=False,
-                        help="""Compress data store.""")
-    parser.add_argument('-n', '--dry-run', action='store_true', default=False,
-                        help="""Print a representation of the input data and exit""")
+    parser.add_argument('-c', '--no-compress', action='store_false', default=True, dest='compress',
+                        help="""Don't compress data store.""")
 
 
 def action(args):
-
 
     try:
         import tables
@@ -52,15 +50,22 @@ def action(args):
         outfile = path.join(outdir, path.splitext(infile)[0]) + '.hdf5'
 
     tab = pd.read_csv(args.infile,
-                      header=None if args.headless else 0)
+                      header=None if args.no_header else 0)
 
     fieldnames = args.fieldnames.split(',') if args.fieldnames else None
     if fieldnames:
         tab.rename(columns=dict(zip(tab.columns, fieldnames)), inplace=True)
 
-    if args.dry_run:
+    # tab[[2, 6]] = tab[[2, 6]].astype(np.float16)
+    # tab[[3, 4, 5]] = tab[[3, 4, 5]].astype(np.int16)
+    # tab[[0, 1]] = tab[[0, 1]].astype(pd.Categorical)
+
+    if args.verbosity > 1:
         print tab.head()
         print tab.dtypes
-        sys.exit(0)
 
-    tab.to_hdf(outfile, args.key)
+    hdfargs = {}
+    if args.compress:
+        hdfargs.update(dict(complevel=9, complib='bzip2'))
+
+    tab.to_hdf(outfile, args.key, **hdfargs)
