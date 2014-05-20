6f72d3d5a441a8c6b4c1417423d0792e29a9193a
Noah Hoffman <noah.hoffman@gmail.com>
2014-05-19 17:33:53 -0700

    - classifier produces minimal output with mocked-up names - reorganize command line options - add test files for classifier

--- bioy_pkg/subcommands/classifier.py
+++ bioy_pkg/subcommands/classifier.py
@@ -1,7 +1,15 @@
-"""
-Classify sequences by grouping blast output by matching taxonomic names
+"""Classify sequences by grouping blast output by matching taxonomic names
 
 Optional grouping by specimen and query sequences
+
+The output is a table containing the following columns:
+
+specimen, max_percent, min_percent, max_coverage, min_coverage,
+assignment_id, assignment, clusters, reads, pct_read, corrected,
+pct_corrected, target_rank, hi, low, tax_ids
+
+
+
 """
 import sys
 import logging
@@ -22,6 +30,8 @@ from bioy_pkg.utils import Opener, opener, Csv2Dict, groupbyl
 log = logging.getLogger(__name__)
 
 def build_parser(parser):
+
+    # input files
     parser.add_argument(
         'blast_file', help='CSV tabular blast file of query and subject hits.')
     parser.add_argument(
@@ -35,117 +45,130 @@ def build_parser(parser):
         help='File mapping reference seq name to tax_id')
     parser.add_argument(
         '-t', '--taxonomy', required=True, metavar='CSV',
-        help='tax table of taxids and species names')
-
-    parser.add_argument('--all-one-group',
-                        dest = 'all_one_group',
-            action = 'store_true', default=False,
-            help = """If --map is not provided, the default behavior is to treat
-                    all reads as one group; use this option to treat
-                    each read as a separate group [%(default)s]""")
+        help="""Table defining the taxonomy for each tax_id""")
     parser.add_argument(
-        '--starred', default = 100.0, metavar='PERCENT', type = float,
-        help = """Names of organisms for which at least one reference
+        '--copy-numbers', metavar='CSV',
+        help="""Estimated 16s rRNA gene copy number for each tax_ids
+        (CSV file with columns: tax_id, median)""")
+    parser.add_argument(
+        '-g', '--groups', metavar='CSV',
+        help="""CSV file with columns (name, specimen) assigning sequences to
+        groups. The default behavior is to treat all query sequences
+        as belonging to one specimen.""")
+    parser.add_argument(
+        '--exclude-taxids', metavar='File',
+        help="""A file containing taxids - blast hits to reference sequences with
+        these taxids will be discarded.""")
+
+    # input file parameters
+    parser.add_argument(
+        '--has-header', action='store_true', default=False,
+        help='specify this if blast data has a header')
+
+    # output files
+    parser.add_argument(
+        '-o', '--out', default=sys.stdout, type=Opener('w'), metavar='FILE',
+        help="Classification results.")
+    parser.add_argument(
+        '-d', '--details', type=Opener('w'), metavar='FILE',
+        help="""Optional details of taxonomic assignments.""")
+
+    # classification parameters
+    parser.add_argument(
+        '--rank', default='species',
+        help='Rank at which to classify. Default: "%(default)s"')
+    parser.add_argument(
+        '--min-identity', default=99.0, metavar='PERCENT', type=float,
+        help='minimum identity threshold for accepting matches [> %(default)s]')
+    parser.add_argument(
+        '--max-identity', default=100.0, metavar='PERCENT', type=float,
+        help='maximum identity threshold for accepting matches [<= %(default)s]')
+    parser.add_argument(
+        '--starred', default=100.0, metavar='PERCENT', type=float,
+        help="""Names of organisms for which at least one reference
         sequence has pairwise identity with a query sequence of at
         least PERCENT will be marked with an asterisk [%(default)s]""")
-    parser.add_argument('--copy-numbers',
-            metavar = 'CSV',
-            type = Opener(),
-            help = 'columns: tax_id, median')
-    parser.add_argument('-c', '--coverage',
-            default = 95,
-            metavar = 'PERCENT',
-            type = float,
-            help = 'percent of alignment coverage of blast result [%(default)s]')
-    parser.add_argument('--details-identity',
-            metavar = 'PERCENT',
-            help = 'Minimum identity to include blast hits in details file',
-            type = float,
-            default = 90)
-    parser.add_argument('--details-full',
-            action = 'store_true',
-            help = 'do not limit out_details to only larget cluster per assignment')
-    parser.add_argument('--exclude-by-taxid',
-            metavar = 'CSV',
-            type = lambda f: set(e for e in DictReader(opener(f), fieldnames ='tax_id')),
-            default = {},
-            help = 'column: tax_id')
-    parser.add_argument('--group-def',
-            metavar = 'INT',
-            action = 'append',
-            default = [],
-            help = """define a group threshold for a particular rank overriding
-                      --target-max-group-size. example: genus:2""")
-    parser.add_argument('--group-label',
-            metavar = 'LABEL',
-            default = 'all',
-            help = 'Single group label for reads')
-    parser.add_argument('-o', '--out',
-            default = sys.stdout,
-            type = Opener('w'),
-            metavar = 'CSV',
-            help = """columns: specimen, max_percent, min_percent, max_coverage,
-                      min_coverage, assignment_id, assignment, clusters, reads,
-                      pct_reads, corrected, pct_corrected, target_rank, hi, low, tax_ids""")
-    parser.add_argument('-m', '--map',
-            metavar = 'CSV',
-            type = Opener(),
-            default = {},
-            help = 'columns: name, specimen')
-    parser.add_argument('--max-ambiguous',
-            metavar = 'INT',
-            default = 3,
-            type = int,
-            help = 'Maximum ambiguous count in reference sequences [%(default)s]')
-    parser.add_argument('--max-identity',
-            default = 100,
-            metavar = 'PERCENT',
-            type = float,
-            help = 'maximum identity threshold for accepting matches [<= %(default)s]')
-    parser.add_argument('--min-cluster-size',
-            default = 0,
-            metavar = 'INT',
-            type = int,
-            help = 'minimum cluster size to include in classification output')
-    parser.add_argument('--min-identity',
-            default = 99,
-            metavar = 'PERCENT',
-            type = float,
-            help = 'minimum identity threshold for accepting matches [> %(default)s]')
-    parser.add_argument('-O', '--out-detail',
-            type  = lambda f: DictWriter(opener(f, 'w'), extrasaction = 'ignore', fieldnames = [
-                'specimen', 'assignment', 'assignment_id', 'qseqid', 'sseqid', 'pident', 'coverage', 'ambig_count',
-                'accession', 'tax_id', 'tax_name', 'target_rank', 'rank', 'hi', 'low'
-                ]),
-            metavar = 'CSV',
-            help = """columns: specimen, assignment, assignment_id,
-                      qseqid, sseqid, pident, coverage, ambig_count,
-                      accession, tax_id, tax_name, target_rank, rank, hi, low""")
-    parser.add_argument('--target-max-group-size',
-            metavar = 'INTEGER',
-            default = 3,
-            type = int,
-            help = """group multiple target-rank assignments that
-                      excede a threshold to a higher rank [%(default)s]""")
-    parser.add_argument('--rank',
-            metavar='RANK',
-            help = 'Rank at which to classify. Default: "%(default)s"',
-            default = 'species')
-    ### csv.Sniffer.has_header is *not* reliable enough
-    parser.add_argument('--has-header', action = 'store_true',
-            help = 'specify this if blast data has a header')
+    parser.add_argument(
+        '-c', '--coverage', default=95.0, type=float, metavar='PERCENT',
+        help='percent of alignment coverage of blast result [%(default)s]')
+    parser.add_argument(
+        '--min-cluster-size', default=1, metavar='INTEGER', type=int,
+        help='minimum cluster size to include in classification output')
+    parser.add_argument(
+        '--max-ambiguous', metavar='INTEGER', default=3, type=int,
+        help='Maximum ambiguous count in reference sequences [%(default)s]')
+    parser.add_argument(
+        '--all-one-group', action='store_true', default=False,
+        help="""If --map is not provided, the default behavior is to
+        treat all reads as one group; use this option to treat each
+        read as a separate group [%(default)s]""")
+
+    # options for behavior of naming aglorithm
+    parser.add_argument(
+        '--target-max-group-size', metavar='INTEGER', default=3, type=int,
+        help="""group multiple target-rank assignments that excede a
+        threshold to a higher rank [%(default)s]""")
+    parser.add_argument(
+        '--group-def', metavar='INTEGER', action='append',
+        default=[], help="""define a group threshold for a
+        particular rank overriding --target-max-group-size. example:
+        genus:2""")
+
+    # TODO: what does this do?
+    parser.add_argument(
+        '--group-label', metavar = 'LABEL', default = 'all',
+        help = """Single group label for reads""")
+
+    # options for details file
+    parser.add_argument(
+        '--details-identity', metavar='PERCENT', type=float, default=90.0,
+        help='Minimum identity to include blast hits in details file')
+    parser.add_argument(
+        '--details-full', action='store_true', default='False',
+        help='do not limit out_details to only larget cluster per assignment')
 
 
 class Assignment(object):
+    """An instance of the Assignment class is used to create a dictionary
+    of group keys. Each time the assign() method is called, the
+    provided DataFrame 'df' is grouped according to self.colnames and
+    the group keys are added to self.assignment keyed by a hash.
+
+    """
+
     def __init__(self, colnames):
+        """'colnames' is a list of column names on which to group the input of
+        the 'assign' method.
+
+        """
         self.colnames = colnames
         self.assignments = {}
 
     def assign(self, df):
+        """Store the group keys resulting from grouping 'df' by self.colnames
+        in self.assignments and return the hash.
+
+        """
         assignment = frozenset(df.groupby(self.colnames).groups.keys())
-        h = hash(assignment)
-        self.assignments[h] = assignment
-        return h
+        assignment_key = hash(assignment)
+        self.assignments[assignment_key] = assignment
+        return assignment_key
+
+
+def assign_names(assignment, no_result='[no blast result]'):
+    """Mockup of a function to convert a frozenset containing (tax_id,
+    is_starred) tuples into a taxonomic name.
+
+    """
+
+    if assignment:
+        try:
+            return '-'.join(sorted({str(tax_id) for tax_id, _ in assignment}))
+        except TypeError:
+            print assignment
+            sys.exit()
+    else:
+        return no_result
 
 
 def read_csv(filename, compression=None, **kwargs):
@@ -159,6 +182,11 @@ def read_csv(filename, compression=None, **kwargs):
 
     return pd.read_csv(filename, **kwargs)
 
+
+# details columns (maybe)
+# ['specimen', 'assignment', 'assignment_id', 'qseqid', 'sseqid',
+# 'pident', 'coverage', 'ambig_count', 'accession', 'tax_id',
+# 'tax_name', 'target_rank', 'rank', 'hi', 'low']
 
 def action(args):
 
@@ -177,14 +205,22 @@ def action(args):
     taxonomy = read_csv(args.taxonomy, dtype=str)
 
     # format blast data and add additional available information
-    blast_results = read_csv(args.blast_file, header=None)
-    fieldnames = None if args.has_header else sequtils.BLAST_HEADER
-    blast_results.rename(columns=dict(zip(blast_results.columns, fieldnames)),
-                         inplace=True)
+    blast_results = read_csv(
+        args.blast_file, header=0 if args.has_header else None)
 
-    # get rows corresponding to sequences with no blast hits
+    if not args.has_header:
+        # assert len(blast_results.columns) == len(sequtils.BLAST_HEADER)
+        blast_results.rename(
+            columns=dict(zip(blast_results.columns, sequtils.BLAST_HEADER)),
+            inplace=True)
+
+    # debug: keep track of original qseqids here
+    qseqids_in = {x for x in blast_results.qseqid}
+
+    # Create a Series containing sequence names (ie, qseqids) with no
+    # blast hits; represent this state with a value of 0.
     no_hit_rows = blast_results['sseqid'].isnull()
-    no_hits = blast_results[no_hit_rows]
+    no_hits = pd.Series(0, index=blast_results.qseqid[no_hit_rows])
 
     # ... and remove them from blast_results
     blast_results = blast_results[no_hit_rows.apply(lambda x: not x)]
@@ -211,7 +247,6 @@ def action(args):
     hits = pd.merge(hits, taxonomy[['tax_id', 'tax_name']], how='left',
                     left_on=args.rank, right_on='tax_id')
 
-
     # rename the column containing tax_id's at the specified rank to
     # 'tax_id' and delete the duplicate column 'tax_id_y'; tax_id_x
     # now refers to the original tax_ids for the reference sequences.
@@ -223,20 +258,42 @@ def action(args):
 
     grouped_by_query = hits.groupby(['qseqid'])
 
-    assignments = grouped_by_query.apply(assigner.assign)
+    # Determine assignment names and concatenate with sequences having
+    # no blast hit.
+    assigned = pd.concat([no_hits, grouped_by_query.apply(assigner.assign)])
+    assigned.name = 'assignment_key'
 
-    print assignments
+    assert set(assigned.index) == qseqids_in
 
-    # for k, v in assigner.assignments.items():
-    #     print sorted(v)
+    assignments = pd.DataFrame(assigned)
+    assignments['qseqid'] = assignments.index
+
+    if args.weights:
+        weights = read_csv(args.weights, header=None, names=['qseqid', 'weight'])
+        assignments = pd.merge(assignments, weights, how='left', on='qseqid')
+    else:
+        assignments['weight'] = 1
+
+    # add assignments
+    assignments['assignment'] = assignments.assignment_key.map(
+        lambda x: assign_names(assigner.assignments.get(x)))
+
+    # group by assignment
+    counts = assignments.groupby(['assignment']).weight.sum()
+    counts.name = 'reads'
+
+    output = pd.DataFrame(counts)
+    output['freq'] = output/output.sum()
+    output['clusters'] = assignments.groupby(['assignment']).size()
+
+    output.sort('reads', ascending=False, inplace=True)
+
+    output.to_csv(args.out)
 
     # next steps...
-    # - concatenate with no_hits (corresponding to an assignment of 0? nan?)
-    # - create a DataFrame indexed by qseqid with assignments and additional column 'weights'
-    # - group by assignment and get sum of weights
     # - create names for assignments
     # - calculate and apply copy number correction factor
-    # - order assignments by mass desc and add assignment_id
+    # - add assignment_ids
     # - assemble details
 
 
