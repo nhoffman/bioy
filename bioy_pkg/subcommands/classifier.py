bda2256a75319326e7213b6879a4aabc0f19db3c
Chris Rosenthal <crosenth@uw.edu>
2014-06-19 15:48:51 -0700

    group by specimen

--- bioy_pkg/subcommands/classifier.py
+++ bioy_pkg/subcommands/classifier.py
@@ -24,8 +24,6 @@ log = logging.getLogger(__name__)
 
 
 def build_parser(parser):
-
-    # input files
     parser.add_argument(
         'blast_file', help='CSV tabular blast file of query and subject hits.')
     parser.add_argument(
@@ -85,12 +83,8 @@ def build_parser(parser):
         '--min-cluster-size', default=1, metavar='INTEGER', type=int,
         help='minimum cluster size to include in classification output')
     parser.add_argument(
-        '--all-one-group', action='store_true', default=False,
-        help="""If --map is not provided, the default behavior is to
-        treat all reads as one group; use this option to treat each
-        read as a separate group [%(default)s]""")
-
-    # options for behavior of naming aglorithm
+        '--specimen', metavar='LABEL',
+        help="""Single group label for reads""")
     parser.add_argument(
         '--target-max-group-size', metavar='INTEGER', default=3, type=int,
         help="""group multiple target-rank assignments that excede a
@@ -100,14 +94,6 @@ def build_parser(parser):
         default=[], help="""define a group threshold for a
         particular rank overriding --target-max-group-size. example:
         genus:2""")
-
-    # TODO: what does this do?
-    # CR: text label to handle all blast results as one group when no specimen
-    # map is specified.  Goes with the all-one-group option above
-    parser.add_argument(
-        '--group-label', metavar='LABEL', default='all',
-        help="""Single group label for reads""")
-
     parser.add_argument(
         '--details-full', action='store_true',
         help='do not limit out_details to only larget cluster per assignment')
@@ -130,6 +116,49 @@ def action(args):
     # pd.set_option('display.max_columns', None)
     # pd.set_option('display.max_rows', None)
 
+    # format blast data and add additional available information
+    names = None if args.has_header else sequtils.BLAST_HEADER
+    header = 0 if args.has_header else None
+    usecols = ['qseqid', 'sseqid', 'pident', 'coverage']
+    blast_results = read_csv(args.blast_file,
+                             names=names,
+                             na_filter=True,  # False is faster
+                             header=header,
+                             usecols=usecols)
+
+    # filter out high and low identity hits and low coverage hits
+    blast_results = blast_results[
+        blast_results['pident'] >= args.min_identity]
+    blast_results = blast_results[
+        blast_results['pident'] <= args.max_identity]
+    blast_results = blast_results[
+        blast_results['coverage'] >= args.min_coverage]
+
+    # assign specimen names
+    specimens = blast_results['qseqid'].drop_duplicates()
+    specimens = specimens.reset_index().set_index('qseqid')
+    specimens = specimens.drop('index', axis=1)
+
+    if args.specimen_map:
+        spec_map = read_csv(args.specimen_map,
+                            names=['qseqid', 'specimen'],
+                            index_col='qseqid')
+        specimens = specimens.join(spec_map)
+    elif args.specimen:
+        specimens['specimen'] = args.specimen
+    else:
+        specimens['specimen'] = specimens.index
+
+    blast_results = blast_results.join(specimens, on='qseqid')
+
+    # hi and low
+    blast_results['hi'] = args.max_identity
+    blast_results['low'] = args.min_identity
+
+    # assign target rank
+    # TODO: read in thresholds file
+    blast_results['target_rank'] = args.rank
+
     seq_info = read_csv(
         args.seq_info,
         usecols=['seqname', 'tax_id', 'accession'],
@@ -142,31 +171,6 @@ def action(args):
 
     # must set index after assigning datatype
     taxonomy = read_csv(args.taxonomy, dtype=str).set_index('tax_id')
-
-    # format blast data and add additional available information
-    names = None if args.has_header else sequtils.BLAST_HEADER[:6]
-    header = 0 if args.has_header else None
-    blast_results = read_csv(args.blast_file,
-                             names=names,
-                             na_filter=True,  # False (faster)
-                             header=header,
-                             usecols=['qseqid', 'sseqid', 'pident'])
-
-    # filter out high and low identity hits and low coverage hits
-    blast_results = blast_results[blast_results['pident'] >= args.min_identity]
-    blast_results = blast_results[blast_results['pident'] <= args.max_identity]
-
-    if 'coverage' in blast_results:
-        blast_results = blast_results[
-            blast_results['coverage'] >= args.min_coverage]
-
-    # hi and low
-    blast_results['hi'] = args.max_identity
-    blast_results['low'] = args.min_identity
-
-    # assign target rank
-    # TODO: read in thresholds file
-    blast_results['target_rank'] = args.rank
 
     # merge blast results with seq_info - do this early so that
     # refseqs not represented in the blast results are discarded in
@@ -226,14 +230,16 @@ def action(args):
     # put assignments and no assignments back together
     blast_results = pd.concat([blast_results, no_hits])
 
-    # concludes our blast details
+    # concludes our blast details, on to summarizing output
 
     # now for some assignment grouping and summarizing
-    output = blast_results[['assignment_hash', 'assignment', 'target_rank']]
-    output = output.drop_duplicates().set_index('assignment_hash')
+    output = blast_results[['specimen', 'assignment_hash',
+                            'assignment', 'target_rank']]
+    output = output.drop_duplicates()
+    output = output.set_index(['specimen', 'assignment_hash'])
 
     # the qseqid cluster stats
-    qseqids = blast_results[['qseqid', 'assignment_hash']]
+    qseqids = blast_results[['qseqid', 'specimen', 'assignment_hash']]
     qseqids = qseqids.drop_duplicates().set_index('qseqid')
 
     if args.weights:
@@ -247,10 +253,16 @@ def action(args):
     else:
         qseqids['weight'] = 1
 
-    qseqids = qseqids.groupby(by=['assignment_hash'], sort=False)
+    qseqids = qseqids.groupby(by=['specimen', 'assignment_hash'], sort=False)
 
     output['reads'] = qseqids.weight.sum()
-    output['freq'] = output['reads'] / output['reads'].sum() * 100
+
+    def freq(df):
+        df['freq'] = df['reads'] / df['reads'].sum() * 100
+        return df
+
+    output = output.groupby(level='specimen').apply(freq)
+
     output['clusters'] = qseqids.size()
 
     if args.copy_numbers:
@@ -265,26 +277,35 @@ def action(args):
         copy_numbers = copy_numbers.append(default)
 
         # do our copy number correction math
-        corrections = blast_results[['tax_id', 'assignment_hash']]
+        corrections = blast_results[['tax_id', 'specimen', 'assignment_hash']]
         corrections = corrections.drop_duplicates().set_index('tax_id')
         corrections = corrections.join(copy_numbers)
-        corrections = corrections.groupby(by=['assignment_hash'], sort=False)
+        corrections = corrections.groupby(
+            by=['specimen', 'assignment_hash'], sort=False)
         corrections = corrections['median'].mean()
         output['corrected'] = output['reads'] / corrections
-        pct_corrected = output['corrected'] / output['corrected'].sum() * 100
-        output['pct_corrected'] = pct_corrected
+
+        def pct_corrected(df):
+            df['pct_corrected'] = df['corrected'] / df['corrected'].sum() * 100
+            return df
+
+        output = output.groupby(level='specimen').apply(pct_corrected)
         # reset corrected counts to int
         output['corrected'] = output['corrected'].apply(math.ceil)
         output['corrected'] = output['corrected'].astype(int)
 
     # qseqid hit ranges
     assignment_groups = blast_results.groupby(
-        by=['assignment_hash'], sort=False)
+        by=['specimen', 'assignment_hash'], sort=False)
     output['max_percent'] = assignment_groups['pident'].max()
     output['min_percent'] = assignment_groups['pident'].min()
 
-    output = output.sort('corrected', ascending=False)
-    output.to_csv(args.out, index=False, float_format='%.2f')
+    reads = ['corrected'] if 'corrected' in output else ['reads']
+
+    output = output.reset_index(level='assignment_hash', drop=True)
+    output = output.sort(columns=reads, ascending=False)
+    output = output.sort_index()
+    output.to_csv(args.out, index=True, float_format='%.2f')
 
     # output to details.csv.bz2
     if args.out_details:
@@ -292,7 +313,7 @@ def action(args):
             if args.details_full:
                 blast_results.to_csv(out_details,
                                      header=True,
-                                     index=False,
+                                     index=True,
                                      float_format='%.2f')
             else:
                 largest = qseqids.apply(lambda x: x['weight'].nlargest(1))
