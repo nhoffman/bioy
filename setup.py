3971c1c4c933da2733fe7b21027dd1ab9b61c4b9
Chris Rosenthal <crosenth@uw.edu>
2014-07-17 19:43:54 -0700

    moving to pandas 0.14.1

--- setup.py
+++ setup.py
@@ -5,24 +5,24 @@ from setuptools import setup, find_packages
 try:
     subprocess.check_call(
         'git describe --tags --long > bioy_pkg/data/version',
-        shell = True)
+        shell=True)
 except subprocess.CalledProcessError:
     version = ''
 else:
     with open('bioy_pkg/data/version') as f:
         version = f.read().strip().split('-')[0]
 
-setup(author = 'Noah Hoffman',
-      author_email = 'noah.hoffman@gmail.com',
-      description = 'A collection of bioinformatics tools',
-      name = 'bioy',
-      packages = find_packages(),
-      scripts = ['bioy'],
-      version = version,
-      url = 'https://github.com/nhoffman/bioy',
-      requires = ['python (>= 2.7.5)'],
-      install_requires = [
+setup(author='Noah Hoffman',
+      author_email='noah.hoffman@gmail.com',
+      description='A collection of bioinformatics tools',
+      name='bioy',
+      packages=find_packages(),
+      scripts=['bioy'],
+      version=version,
+      url='https://github.com/nhoffman/bioy',
+      requires=['python (>= 2.7.5)'],
+      install_requires=[
           'numpy==1.8.1',
-          'pandas==0.14.0',
+          'pandas==0.14.1',
           'biopython>=1.6.3'
       ])
