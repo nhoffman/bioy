b66066de5f0f65f8b2294fdb562301dc34152613
Tyler A Land <tland9@uw.edu>
2014-11-06 11:47:52 -0800

    bumping pandas version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(author='Noah Hoffman',
       requires=['python (>= 2.7.5)'],
       install_requires=[
           'numpy>=1.8.1',
-          'pandas>=0.14.1',
+          'pandas>=0.15',
           'biopython>=1.6.3',
           'matplotlib'
       ])
