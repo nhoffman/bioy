defe6297c22609354bfd1f1c92918e66888bd691
Chris Rosenthal <crosenth@uw.edu>
2015-01-11 21:40:48 -0800

    new experimental files to replace csvkit

---
+++ bioy_pkg/subcommands/csvjoin.py
@@ -0,0 +1,91 @@
+# This file is part of Bioy
+#
+#    Bioy is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Bioy is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Bioy.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Merge any number of csv file column wide
+"""
+
+import logging
+import sys
+
+from bioy_pkg import utils
+
+log = logging.getLogger(__name__)
+
+
+def build_parser(parser):
+    # required inputs
+    parser.add_argument(
+        'csv',
+        metavar='CSVs',
+        nargs='+',
+        help='tabulated input file')
+
+    # common outputs
+    parser.add_argument(
+        '-o', '--out', metavar='FILE',
+        default=sys.stdout, type=utils.Opener('w'),
+        help="out delimited file")
+
+    parser.add_argument(
+        '--how',
+        choices=['inner', 'outer', 'left', 'right'],
+        default='inner',
+        help=('how to join %(choices)s'))
+    parser.add_argument(
+        '--on',
+        help=('column delimite list of columns or single common column name'))
+    parser.add_argument(
+        '--comment', metavar='CHAR',
+        help=('comment character'))
+    parser.add_argument(
+        '--no-header',
+        action='store_true',
+        help='if first line is data')
+
+
+def action(args):
+    # for debugging:
+    # pandas.set_option('display.max_columns', None)
+    # pd.set_option('display.max_rows', None)
+
+    if args.on:
+        on = args.on.split(',')
+
+        if args.no_header:
+            on = map(int, on)
+    else:
+        on = [None]
+
+    if len(args.csv) != len(on):
+        on = on * len(args.csv)
+
+    dfs = []
+
+    for col, csv in enumerate(args.csv):
+        df = utils.read_csv(
+            csv,
+            dtype=str,
+            index_col=on[col],
+            comment=args.comment,
+            na_filter=False,
+            header=None if args.no_header else 0)
+        dfs.append(df)
+
+    df = dfs[0].join(dfs[1:], how=args.how, lsuffix='_x', rsuffix='_y')
+
+    df.to_csv(
+        args.out,
+        header=not args.no_header,
+        index=bool(args.on))
